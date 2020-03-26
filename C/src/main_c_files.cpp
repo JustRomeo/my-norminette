575811ae38faea8ad4405aba51d92df6efe3aee3
Roméo <romeo@localhost.localdomain>
2020-03-26 10:44:52 +0100

    [ADD] Ajout de plusieurs règles en c et cpp

--- C/src/main_c_files.cpp
+++ C/src/main_c_files.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 string *openFile(string str);
 
+static int _functions = 0;
 static bool _write = false;
 static bool _printf = false;
 
@@ -13,20 +14,32 @@ static void printer_error(string str, int line) {
     printf("%sl.%d\n", str.c_str(), line);
 }
 
+static void proto(string *tab, int indexe) {
+    size_t i = indexe;
+
+    _functions ++;
+    for (; tab[i] != "}"; i ++);
+    if (i - indexe > 21)
+        printf("/!\\ Too long functions %d > 20.\n", i - indexe);
+}
+
 //tab[i].replace(tab[i].find(";;"), sizeof(";;") - 1, ";");
 int norminette_c(string *tab) {
     for (int i = 0; tab[i] != ""; i ++) {
-        if (tab[i].find(" printf") != string::npos) _printf = true;
         if (tab[i].find(" write") != string::npos) _write = true;
+        if (tab[i].find(" printf") != string::npos) _printf = true;
         if (tab[i].find(";;") != string::npos) printer_error("/!\\ Info: \";;\"", i);
-        if (tab[i].find("char *") != string::npos) printer_error("/!\\ \"char *\"", i);
+        if (tab[i].find("char*") != string::npos) printer_error("/!\\ \"char*\" should be \"char *\"", i);
         if (tab[i].find(" ;") != string::npos) printer_error("/!\\ \" ;\" do not need space before ;", i);
         if (tab[i].find("){") != string::npos) printer_error("/!\\ Info: \'{\' should be separate by a space", i);
+        if (tab[i][0] != ' ' && tab[i].find("(") != string::npos && tab[i].find("{") != string::npos) proto(tab, i);
         if (tab[i].find("\t") != string::npos) printer_error("/!\\ Info: \"\t\" better to use \"    \" than a tab", i);
         if (tab[i].length() > 80) printer_error("/!\\ Error: max line length " + to_string(tab[i].length())  + " > 80", i);
     }
 
     if (_write && _printf)
-        printf("/!\\ You are using write AND printf in your program\n");
+        printf("/!\\ You are using write AND printf in your program.\n");
+    if (_functions > 5)
+        printf("/!\\ %d functions in the file, max norme complient it's 5.\n");
     return 0;
 }
--- HPP/src/main_hpp_files.cpp
+++ HPP/src/main_hpp_files.cpp
@@ -5,6 +5,7 @@ using namespace std;
 string *openFile(string str);
 
 static int classe = 0;
+static bool shield = false;
 
 static void printer_error(string str, int line) {
     for (int i = str.length(); i < 75; i ++)
@@ -16,7 +17,13 @@ static void printer_error(string str, int line) {
 int norminette_hpp(string *tab) {
     for (int i = 0; tab[i] != ""; i ++) {
         if (tab[i].find("class") != string::npos && tab[i].find("{") != string::npos) classe ++;
-        if (classe > 0) printer_error("/!\\ Error: only one class per file is allow", i);
+        if (tab[i].find("#ifndef") != string::npos && tab[i + 1].find("#define") != string::npos) shield = true;
+        if (tab[i].find("\t") != string::npos) printer_error("/!\\ Info: \"\t\" better to use \"    \" than a tab", i);
     }
+
+    if (classe > 0)
+        printf("/!\\ Error: only one class per file is allow, there are %d class in the file.\n", classe);
+    if (!false)
+        printf("/!\\ Info: no protection on prototype file, better to protect it.\n");
     return 0;
 }
